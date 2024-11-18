6a0645961967ae3276c0e8fa9bb60bac56b3b84f
sofia_v <sofi.sonia2609@gmail.com>
2024-11-18 13:54:24 +0200

    final commit

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  const res = Object.keys(obj);
+  for (const key of res) {
+    const value = obj[key];
+    callback(key, value, obj);
+  }
+};
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (let i = 0; i < args.length; i++) {
+    const a = args[i];
+    const type = types[i];
+    const name = type.name.toLowerCase();
+    if (typeof a !== name) {
+      throw new TypeError(`Argument type ${name} expected`);
+    }
+  }
+  const res = fn(...args);
+  const type = types[types.length - 1];
+  const name = type.name.toLowerCase();
+  if (typeof res !== name) {
+    throw new TypeError(`Result type ${name} expected`);
+  }
+  return res;
+};
 
 module.exports = { contract };
